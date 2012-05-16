bf5c68416bbf46c852a7d2d163d477153d767ea3
huynhjl <jlh276-gh@yahoo.com>
2012-05-15 22:38:29 -0700

    Adding PNG reading example with maps and flatmaps.

--- app/util/IterUtils.scala
+++ app/util/IterUtils.scala
@@ -3,6 +3,9 @@ package util
 import play.api.libs.iteratee._
 import play.api.Logger
 
+/**
+ * Machinery to print out Inputs.
+ */
 trait Show[E] {
   def str(e: E): String
   def strEl(e: Input[E]): String = e match {
@@ -35,5 +38,55 @@ object Show {
       seq.map(sh.str(_)).mkString("[", ", ", "]")
     }
   }
-
 }
+
+/**
+ * Small example using flatMap and map to combine iteratees.
+ */
+object PNG {
+  
+  val show = implicitly[Show[AB]]
+
+  def get(n: Int) = Traversable.take[AB](n) &>> Iteratee.consume[AB]()
+  
+  val getInt: Iteratee[AB, Int] = get(4).map{ arr => 
+    arr(0)<<24 | arr(1)<<16 | arr(2)<<8 | arr(3)
+  }
+
+  val PNG_HDR = Array(0x89, 'P', 'N', 'G', '\r', '\n', 0x1A, '\n').map(_.toByte)
+
+  val header = get(8).flatMap { arr =>
+    if (arr.sameElements(PNG_HDR)) Done(true, Input.Empty)
+    else Error("invalid PNG header", Input.EOF)
+  }
+
+  val chunkType: Iteratee[AB, String] = get(4).map(show.str)
+
+  def chunkCRC = getInt
+
+  def chunk = for {
+    length <- getInt
+    tpe <- chunkType
+    data <- get(length)
+    crc <- getInt
+  } yield {
+    (length, tpe)
+  }
+
+  def png = for {
+    h <- header
+    chunks <- Iteratee.repeat(chunk)
+  } yield {
+    chunks
+  }
+  
+  def read(url: String) = {
+    val in = new java.net.URL(url).openStream()
+    val source = Enumerator.fromStream(in, 1024)
+    val promise = Iteratee.flatten(source |>> png).run
+    val result = promise.await.get
+    result
+  }
+}
+  
+
